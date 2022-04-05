37b4ce21d5d5aca479bce7dee63db2e4c278da48
MohammadFaysal731 <www.faysal731@gmail.com>
2022-04-05 15:06:52 +0600

    set 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './NotFound.css';
 const NotFound = () => {
     return (
-        <div>
-            <h1>not found</h1>
+        <div className='page-404'>
+            <img src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg" alt="" />
         </div>
     );
 };
