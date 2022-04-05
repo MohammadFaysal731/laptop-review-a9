c5a093a84e385632f456bb7643090e85f39560e6
MohammadFaysal731 <www.faysal731@gmail.com>
2022-04-05 12:24:49 +0600

    desinge home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Home.css';
 const Home = () => {
     return (
-        <div>
-            <h1>home</h1>
+        <div className='home-container'>
+            <div className="home-left">
+                <h3>HP ProBook 440 GB -Wolf Pro Security Edition</h3>
+                <p>Windows 10 pro( available through downgrade rights from Windows 11 pro).</p>
+                <p>11<sup>th</sup>Generation Intel<sup>&copy;</sup>Core <sup>TM</sup>processor.</p>
+                <p>16 GB memory; 5112 GB SSD storage.</p>
+                <p>14<sup>"</sup>diagonal FHD display.</p>
+
+            </div>
+            <div className="home-right">
+                <img src="https://www.nicepng.com/png/detail/2-25536_hp-laptop-png-image-background-hp-probook-440.png" alt="" />
+            </div>
         </div>
     );
 };
